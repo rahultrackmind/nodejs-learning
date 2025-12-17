d7eab0464244e8567961cb658581fe176df1574e
Rahul Sharma <rahul@trackmind.com>
2025-12-17 18:02:26 +0530

    changes

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,6 +3,6 @@ class ApiResponse {
         this.statusCode = statusCode;
         this.data = data;
         this.message = message;
-        this.success = statusCode < 400; //for response related status codes
+        this.success = statusCode < 400; //for  response client related status codes
     }
 }
